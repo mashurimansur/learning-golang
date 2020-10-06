983f92922c43e9a76481a0612280e6c0f516d769
Hury <mashurimansur18@gmail.com>
2020-10-06 20:15:51 +0800

    update channel.go

--- channel.go
+++ channel.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+)
 
+func printMessage(what chan string) {
+	fmt.Println(<-what)
+}
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	var messages = make(chan string)
+
+	for _, each := range []string{"wick", "hunt", "bourne"} {
+		go func(who string) {
+			var data = fmt.Sprintf("hello %s", who)
+			messages <- data
+		}(each)
+	}
+
+	for i := 0; i < 3; i++ {
+		printMessage(messages)
+	}
 }
