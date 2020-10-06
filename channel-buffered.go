3d0dd3cf66263f22a93566759974055cb8a4581d
Hury <mashurimansur18@gmail.com>
2020-10-06 20:23:41 +0800

    update channel-buffered.go

---
+++ channel-buffered.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	messages := make(chan int, 2)
+
+	go func() {
+		for {
+			i := <-messages
+			fmt.Println("receive data", i)
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("send data", i)
+		messages <- i
+	}
+}
